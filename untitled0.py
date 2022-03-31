94ddb4cb0b756546b80a3da6e817b5ecbd3cccbd
Titus <tituschu@uchicago.edu>
2022-03-31 16:02:22 -0500

    Steps for GitHub1
    Steps for GitHub

--- untitled0.py
+++ untitled0.py
@@ -4,6 +4,13 @@
 Created on Wed Mar 30 13:10:13 2022
 
 @author: tituschu
+
+Step 1: Create Repo
+Step 2: Clone Locally. Cloning other people's Repo
+Step 3: Download or create our own .py and save to GitHub
+Step 4: Commit to local repo. Add to Main with comments
+Step 5: Push to Remote Repo
+
 """
 
 print('Lecture 1')
